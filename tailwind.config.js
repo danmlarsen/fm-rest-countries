9c72d71d12c1c1b6b3bf4d677ee1a3c36a800445
danmlarsen <larsendanmarius@gmail.com>
2024-10-28 09:34:58 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
